3b56f75489497e5329f05415f01fc3a1231c95ec
KhadizaSamiha <khadizasamiha7@gmail.com>
2023-05-07 23:14:33 +0600

    route setup

---
+++ src/Pages/Blog.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Blog = () => {
+    return (
+        <div>
+            This is bloggg
+        </div>
+    );
+};
+
+export default Blog;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,40 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Main from './Layout/Main.jsx';
+import Home from './Pages/Home';
+import Blog from './Pages/Blog';
+import Contact from './Pages/Contact';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+    children:[
+      {
+        path:'/',
+        element:<Home></Home>
+      },
+      {
+        path:'/blog',
+        element:<Blog></Blog>
+      },
+      {
+        path:'/contact',
+        element:<Contact></Contact>
+      },
+
+    ]
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
