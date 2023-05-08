7ba2cdc5e304b3212228afc024cb1fa7003ed12d
KhadizaSamiha <khadizasamiha7@gmail.com>
2023-05-09 01:30:47 +0600

    lazy load

--- src/Pages/Chef.jsx
+++ src/Pages/Chef.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-const Chef = ({chef}) => {
-    const {id, name, experience, likes, image, recipes} = chef;
+import LazyLoad from 'react-lazy-load';
+
+const Chef = ({ chef }) => {
+    const { id, name, experience, likes, image, recipes } = chef;
     return (
         <div className="card w-80 bg-base-100 shadow-xl text-white mx-auto ">
-            <figure><img src={image} alt="Shoes" /></figure>
+            <figure><LazyLoad >
+            <img src={image} />
+            </LazyLoad></figure>
             <div className="card-body bg-slate-500">
                 <h2 className="card-title text-3xl">{name}</h2>
                 <p>Experience : {experience} years</p>
                 <p>Recipies : {recipes}</p>
                 <p>Likes : {likes} years</p>
                 <div className="card-actions justify-end">
-                   <Link to={`/chef/${id}`} ><button className="btn btn-outline btn-error">View Recipies</button></Link>
+                    <Link to={`/chef/${id}`} ><button className="btn btn-outline btn-error">View Recipies</button></Link>
                 </div>
             </div>
         </div>
