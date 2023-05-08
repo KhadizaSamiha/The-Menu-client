5b3920942aab63950dddfd302159d56e355577af
KhadizaSamiha <khadizasamiha7@gmail.com>
2023-05-08 18:24:03 +0600

    blog done

--- src/Pages/Blog.jsx
+++ src/Pages/Blog.jsx
@@ -3,7 +3,40 @@ import React from 'react';
 const Blog = () => {
     return (
         <div>
-            This is bloggg
+            <div tabIndex={0} className="collapse collapse-arrow border border-base-300 bg-base-100 rounded-box">
+                <div className="collapse-title text-xl font-medium">
+                1. Difference between control and Uncontrolled component in react
+                </div>
+                <div className="collapse-content">
+                    <p>In React, controlled components refer to components that have their state and behavior controlled by the parent component. These components rely on props passed down from the parent component to update their state and behavior. Uncontrolled components refer to components that manage their own state internally</p>
+                </div>
+            </div>
+            <div tabIndex={0} className="collapse collapse-arrow border border-base-300 bg-base-100 rounded-box">
+                <div className="collapse-title text-xl font-medium">
+                2. How to validate React props using PropTypes?
+                </div>
+                <div className="collapse-content">
+                    <p>any : The prop can be of any data type. bool : The prop should be a Boolean. number : The prop should be a number. string : The prop should be a string. func : The prop should be a function. array : The prop should be an array. object : The prop should be an object.</p>
+                </div>
+            </div>
+            <div tabIndex={0} className="collapse collapse-arrow border border-base-300 bg-base-100 rounded-box">
+                <div className="collapse-title text-xl font-medium">
+                3. Difference between nodejs and express js?
+                </div>
+                <div className="collapse-content">
+                    <p>Express is a minimal and flexible node. js web application framework, providing a robust set of features for building single and multi-page, and hybrid web applications. On the other hand, Node. js is detailed as "A platform built on Chrome's JavaScript runtime for easily building fast, scalable network applications".</p>
+                </div>
+            </div>
+            <div tabIndex={0} className="collapse collapse-arrow border border-base-300 bg-base-100 rounded-box">
+                <div className="collapse-title text-xl font-medium">
+                4. What is a custom hook, and why will you create a custom hook?
+                </div>
+                <div className="collapse-content">
+                    <p>Custom React JS hooks offer reusability as when a custom hook is created, it can be reused easily, which makes the code cleaner and reduces the time to write the code. It also enhances the rendering speed of the code as a custom hook does not need to be rendered again and again while rendering the whole code.
+
+</p>
+                </div>
+            </div>
         </div>
     );
 };
